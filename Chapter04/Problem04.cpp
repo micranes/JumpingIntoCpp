18351344870176e88dd58308e76ccb39b0e5c3e4
micranes <micranes@hotmail.com>
2025-06-19 12:49:14 -0500

    Finished Problem 4

--- Chapter04/Problem04.cpp
+++ Chapter04/Problem04.cpp
@@ -7,19 +7,67 @@
      Think about how easy (or hard) it is to do this for a lot of usernames and passwords.
 */
 #include<iostream>
+#include<string>
+using namespace std;
 
 int main()
 {
-    int userNumber;
+    
+    string userOne="Bob";
+    string userTwo="John";
+    string userThree="Job";
 
-    std::cout<<"\n\nEnter the code: ";
-    std::cin>>userNumber;
-    if (userNumber==231||userNumber==123)
-    {
-        std::cout<<"\n\nCode Correct!!!\n\n";
+    string pswdOne="hey";
+    string pswdTwo="heyhey";
+    string pswdThree="heyheyhey";
+
+    string userName;
+    string userPswd;
+
+    cout<<"\n\nWho are you: ";
+    cin>>userName;
+    if(!(userName==userOne || userName==userTwo || userName==userThree)){
+        cout<<"\nNot a valid user! Please try again: \n";
+        cin>>userName;
+        if(!(userName==userOne || userName==userTwo || userName==userThree)){
+            cout<<"\nNot a valid user! Get out of here!\n";
+            return 1;
+        }
     }
-    else
-    {
-        std::cout<<"\n\nIncorrect code you villain!!!\n\n";
+
+    cout<<"\n\nHello "<<userName<<" What is your password: ";
+    cin>>userPswd;
+    if(userName==userOne){
+        if(userPswd!=pswdOne){
+            cout<<"\nInvalid password! Try again loser.\n";
+            cin>>userPswd;
+            if(userPswd!=pswdOne){
+                    cout<<"\nInvalid password!\n";
+                    return 1;
+            }    
+            
+        }        
+    }else if (userName==userTwo){
+        if(userPswd!=pswdTwo){
+            cout<<"\nInvalid password! Try again loser.\n";
+            cin>>userPswd;
+            if(userPswd!=pswdTwo){
+                    cout<<"\nInvalid password!\n";
+                    return 1;
+            }
+        }        
+    }else if(userName==userThree){
+        if(userPswd!=pswdThree){
+            cout<<"\nInvalid password! Try again loser.\n";
+            cin>>userPswd;
+            if(userPswd!=pswdThree){
+                    cout<<"\nInvalid password!\n";
+                    return 1;
+            }
+        }        
     }
+    cout<<"\n\nYou are in! The money is yours.\n\n";
+    return 0;
 }
+
+
