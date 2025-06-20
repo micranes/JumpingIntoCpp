7f6cc37c71301c47c3531849325ce1a8e4d8871c
micranes <micranes@hotmail.com>
2025-06-20 10:43:17 -0500

    Program 4 operational.

--- Chapter05/Problem04.cpp
+++ Chapter05/Problem04.cpp
@@ -2,3 +2,23 @@
 4. Write a password prompt that gives a user only a certain number of password entry attempts—
 so that the user cannot easily write a password cracker.
 */
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    string password = "scorpius";
+    string userInput;
+
+    cout<<"\n\nHi there Harvey\n";
+    for(int i=0;i<3;++i){
+        cout<<"Enter your login: ";
+        cin>>userInput;
+        if(userInput==password){
+            cout<<"\nCome on in Harvey";
+            break;
+        }
+        cout<<"Wrong password whomever you are.\n";
+    }
+}
