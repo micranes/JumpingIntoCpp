c6dea16f3bfec5ec2bc63d7ef474f1cbc731f585
micranes <micranes@hotmail.com>
2025-06-20 10:50:44 -0500

    Question 5 answered.

--- Chapter05/Problem05.cpp
+++ Chapter05/Problem05.cpp
@@ -2,3 +2,12 @@
 5. Try writing each practice problem with each kind of loop—notice which loops work well for each
 kind of problem.
 */
+/*
+This is kind of a wasted problem. 
+
+The answer is generally "while" loops for things you don't know how many times you need a loop. Menus, games and such.
+
+"for" loops for when you know what the count is going to be.alignas
+
+"do while" loops when you need to make sure the loop code executes at least one time.
+*/
