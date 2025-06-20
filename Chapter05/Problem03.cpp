1b4068e3f73d86955bb79335ed37cf3cf448eff1
micranes <micranes@hotmail.com>
2025-06-20 09:33:47 -0500

    Program 3 complete.

--- Chapter05/Problem03.cpp
+++ Chapter05/Problem03.cpp
@@ -2,3 +2,20 @@
 3. Write a program that computes a running sum of inputs from the user, terminating when the
 user gives an input value of 0
 */
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    float newNumber =0;
+    bool loopSwitch = 1;
+    float summedUp=0;
+    
+    while(loopSwitch){
+        cout<<"\n\nEnter a number: ";
+        cin>>newNumber;
+        summedUp=summedUp+newNumber;
+        cout<<"\nSum is "<<summedUp;
+        if (newNumber==0){loopSwitch=false;}
+    }
+}
