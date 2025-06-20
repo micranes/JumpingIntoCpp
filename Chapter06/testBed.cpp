ebcc0d32b6492778cc4f2ffa3e3d5b4f7e228503
micranes <micranes@hotmail.com>
2025-06-20 13:02:40 -0500

    Simple function added to testBed

--- Chapter06/testBed.cpp
+++ Chapter06/testBed.cpp
@@ -2,11 +2,16 @@
 //This chapter is on functions.
 
 #include<iostream>
+using namespace std;
 
+int add (int x,int y);
 
 int main(){
-    
-
-
+    cout<<"\n\n"<<add(2,5)<<"\n\n\n";
     return 0;
+}
+
+int add (int x, int y)
+{
+    return x+y;
 }
