f5189cd65e3dd73f6214662cea2361afd95ec4c1
micranes <micranes@hotmail.com>
2025-06-20 10:57:28 -0500

    Program 6 operational

--- Chapter05/Problem06.cpp
+++ Chapter05/Problem06.cpp
@@ -1,3 +1,13 @@
 /*
 6. Write a program that displays the first 20 square numbers.
 */
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    for(int i = 1;i<21;++i){
+        cout<<"\n"<<i<<" =\t"<<i*i;
+    }
+    cout<<"\n\n";
+}
