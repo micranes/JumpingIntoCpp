09bba8856fa0c5e62a3ec2746434eeb763a100b4
micranes <micranes@hotmail.com>
2025-06-21 08:41:10 -0500

    Program 1 started user input function built

--- Chapter07/Program01.cpp
+++ Chapter07/Program01.cpp
@@ -2,3 +2,45 @@
 1. Implement the source code that turns numbers into English text for numbers between -2 billion
 and 2 billion.
 */
+//one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,thirteen,fourteen,fifteen,sixteen,seventeen,eighteen,nineteen
+//twenty,thirty,fourty,fifty,sixty,seventy,eighty,ninety
+//hundred,thousand,million,billion 
+//negative
+#include<iostream>
+#include<string>
+#include<limits>
+
+using namespace std;
+
+int userinput();
+string numberToText(int sourceNumber);
+
+int main(){
+    int userNumber=userinput();
+    
+    cout<<"\nHere is your answer.\n"<<numberToText(userInput);
+
+}
+
+string numberToText(int sourceNumber){
+
+
+}
+
+int userinput(){
+    int input;
+    cout<<"Enter a number you want converted to text: ";
+    cin>>input;
+    while (true)
+    {
+        if(cin.fail()){
+            cout<<"invalid input, please enter a number. ";
+            cin.clear(); //Reset fail state.
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');//clear buffer
+        } else{
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');//clear left over input
+            return input; //valid integer, exit function
+        }
+    }
+    
+}
