d3f7bd51e48d868f74e2c879cf1259f2237837ca
micranes <micranes@hotmail.com>
2025-06-23 14:11:02 -0500

    Did another refactor on the whole program. Cleaning up function names to make things more readable.

--- Chapter07/Program01.cpp
+++ Chapter07/Program01.cpp
@@ -15,9 +15,10 @@ using namespace std;
 
 int userinput();
 string numberToText(int inputInt);
-string hundredsConvert(int inputInt);
-string oneThru99(string numberString);
-string oneThru999(string numberString);
+string hundreds(string numberString);
+string Thousands(string numberString);
+string millions(string numberString);
+string billions(string numberString);
 
 
 //-------------START MAIN--------------
@@ -63,68 +64,59 @@ string numberToText(int inputInt){
     if(inputInt<0){buildString="negative "; inputInt=abs(inputInt);}//grabs the "-" from the int if needed, and converts the int to a positive.
     string numberString = to_string(inputInt); //This is the integer converted to a string format.
 
-    if(inputInt>0 && inputInt<100)
-    {
-        buildString = buildString + oneThru99(numberString);
-
-    }else if (inputInt>99&&inputInt<1000)
-    {
-        buildString = buildString + oneThru999(numberString);
-    
-    }else if (inputInt>999&&inputInt<1000000)
-    {
-        /* code */
-    
-    }else
-    {
-        /* code */
-    }
+    if(inputInt>0 && inputInt<100){buildString = buildString + hundreds(numberString);}
+    else if (inputInt>99&&inputInt<1000){buildString = buildString + Thousands(numberString);}
+    else if (inputInt>999&&inputInt<1000000){buildString = buildString + millions(numberString);}
+    else    { /* code */  }
     return buildString;
 }
 
 
 /**
- * @brief Converts an integer (0-99) to its text equivalent.
- * @name oneThru99
+ * @brief Converts an integer (1-99) to its text equivalent.
+ * @name hundreds
  * @param String integerString - a number between 1 and 99 in string format.
  * @return A string containing the text representation (e.g., "one" for 1).
  * @pre num must be between 0 and 99, inclusive.
- * @post Returns an empty string if the input is invalid.
+ * @post Returns an empty string if the input is zero.
  * @throws std::invalid_argument if num is out of range.
  */
-string oneThru99(string numberString){
-    if (numberString.size()>2)throw invalid_argument("oneThru99() received too large a string.");
+string hundreds(string numberString){
+    if (numberString.size()>2)throw invalid_argument("hundreds() received too large a string.");
     string buildString;
 
      const string below20[] = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine","ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen","seventeen", "eighteen", "nineteen"};
      const string above20[] = {"","","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
 
-     int numValue = stoi(numberString);
-     if (numValue<20){buildString = below20[stoi(numberString)];}
+     if (stoi(numberString)<20){
+        buildString = below20[stoi(numberString)];}
      else{
-        int tens = numberString[0]-'0';
-        int ones = numberString[1]-'0';
-        buildString = above20[tens] + " " + below20[ones];
+        buildString = above20[numberString[0]-'0'] + " " + below20[numberString[1]-'0'];
      }
     return buildString;
-    
 }
 
 /**
- * @brief Converts an integer (1-999) to its text equivalent.
- * @name oneThru999
+ * @brief Converts string (1-999) to its text equivalent.
+ * @name Thousands
  * @param String integerString - a number between 1 and 999 in string format.
  * @return A string containing the text representation (e.g., "one" for 1).
  * @pre num must be between 1 and 99, inclusive.
- * @post Returns an empty string if the input is invalid.
- * @throws std::invalid_argument if num is out of range.
+ * @post 
+ * @throws 
  */
-string oneThru999(string numberString){
+string Thousands(string numberString){
     string buildString;
-//    string hundredsString = numberString.substr(0,1);//grabs the first digit of the input
-//    string onesAndTens = numberString.substr(1,2);//grabs the leftover digits which are 1-99
-//    onesAndTens = oneThru99(onesAndTens);
-    buildString = oneThru99(numberString.substr(0,1)) + " hundred " + oneThru99(numberString.substr(1,2));
+    buildString = hundreds(numberString.substr(0,1)) + " hundred " + hundreds(numberString.substr(1,2));
+    return buildString;
+}
 
+string millions(string numberString){
+    string buildString;
+    return buildString;
+}
+
+string billions(string numberString){
+    string buildString;
     return buildString;
 }
